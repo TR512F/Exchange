62e031f12185ac90baededffd288038f6d46bc55
Dmytro Bubnov <bubnov.dm@gmail.com>
2024-12-16 16:07:27 +0200

    Update UserService and AdminController

--- src/main/java/com/bubnov/v5/controller/AdminController.java
+++ src/main/java/com/bubnov/v5/controller/AdminController.java
@@ -1,11 +1,9 @@
 package com.bubnov.v5.controller;
 
 import com.bubnov.v5.model.Role;
-import com.bubnov.v5.model.User;
-import com.bubnov.v5.repository.UserRepository;
+import com.bubnov.v5.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,37 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AdminController {
 
-    private UserRepository userRepository;
+    private UserService userService;
 
     @PutMapping("/block/{username}")
     public ResponseEntity<?> blockUser(@PathVariable String username) {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        user.setLocked(true);
-        userRepository.save(user);
+        userService.blockUser(username, true);
         return ResponseEntity.ok("User blocked successfully");
     }
 
     @PutMapping("/unblock/{username}")
     public ResponseEntity<String> unblockUser(@PathVariable String username) {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        user.setLocked(false);
-        userRepository.save(user);
+        userService.blockUser(username, false);
         return ResponseEntity.ok("User unblocked successfully");
     }
 
     @PutMapping("/set-admin/{username}")
     public ResponseEntity<?> setAdmin(@PathVariable String username) {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        user.setRole(Role.ROLE_ADMIN);
-        userRepository.save(user);
+        userService.setRole(username, Role.ROLE_ADMIN);
         return ResponseEntity.ok("Role Admin set successfully successfully");
     }
 
     @PutMapping("/set-user/{username}")
     public ResponseEntity<String> setUser(@PathVariable String username) {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        user.setRole(Role.ROLE_USER);
-        userRepository.save(user);
+        userService.setRole(username, Role.ROLE_USER);
         return ResponseEntity.ok("Role User set successfully successfully");
     }
 }
--- src/main/java/com/bubnov/v5/service/UserService.java
+++ src/main/java/com/bubnov/v5/service/UserService.java
@@ -4,19 +4,21 @@ import com.bubnov.v5.model.Role;
 import com.bubnov.v5.model.User;
 import com.bubnov.v5.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService{
+
     private final UserRepository repository;
+
+    public UserDetailsService userDetailsService() {
+        return this;
+    }
+
     public User save(User user) {
         return repository.save(user);
     }
@@ -31,34 +33,21 @@ public class UserService implements UserDetailsService{
         return save(user);
     }
 
-    public User getByUsername(String username) {
-        return repository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public void setRole(String username, Role role) {
+        User user = (User) loadUserByUsername(username);
+        user.setRole(role);
+        repository.save(user);
     }
 
-    public UserDetailsService userDetailsService() {
-        return this::getByUsername;
+    public void blockUser(String username, Boolean isLocked) {
+        User user = (User) loadUserByUsername(username);
+        user.setLocked(isLocked);
+        repository.save(user);
     }
-
-    public User getCurrentUser() {
-        var username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return getByUsername(username);
-    }
-
-    public void getAdmin() {
-        var user = getCurrentUser();
-        user.setRole(Role.ROLE_ADMIN);
-        save(user);
-    }
-
-    private static final Map<String, UserDetails> USERS = new HashMap<>();
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails user = USERS.get(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found: " + username);
-        }
-        return user;
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
